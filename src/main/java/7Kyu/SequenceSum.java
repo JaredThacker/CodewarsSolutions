57a637afab783bc3e14ba381b78836f0b4187042
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-24 15:44:02 -0400

    sequencesum solution

---
+++ src/main/java/7Kyu/SequenceSum.java
@@ -0,0 +1,12 @@
+public class SequenceSum {
+    public static int sequenceSum(int start, int end, int step) {
+        if (start>end){
+            return 0;
+        }
+        int sum = 0;
+        for (int i = start; i <= end; i += step){
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/SequenceSumTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// TODO: Replace examples and use TDD by writing your own tests
+
+class SolutionTest {
+    @Test
+    @DisplayName("Sample Test Cases")
+    void testSomething() {
+        assertEquals(12, SequenceSum.sequenceSum( 2,  6, 2), "sequenceSum(2, 6, 2)");
+        assertEquals(15, SequenceSum.sequenceSum( 1,  5, 1), "sequenceSum(1, 5, 1)");
+        assertEquals( 5, SequenceSum.sequenceSum( 1,  5, 3), "sequenceSum(1, 5, 3)");
+        assertEquals(45, SequenceSum.sequenceSum( 0, 15, 3), "sequenceSum(0, 15, 3)");
+        assertEquals( 0, SequenceSum.sequenceSum(16, 15, 3), "sequenceSum(16, 15, 3)");
+    }
+}
