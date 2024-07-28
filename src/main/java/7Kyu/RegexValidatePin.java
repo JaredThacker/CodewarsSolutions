b0f1624362d209056081c08149e32fd88e043f83
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-28 13:13:05 -0400

    refactor regex pin solution

--- src/main/java/7Kyu/RegexValidatePin.java
+++ src/main/java/7Kyu/RegexValidatePin.java
@@ -1,8 +1,5 @@
 public class RegexValidatePin {
     public static boolean validatePin(String pin) {
-        if (pin.matches("[0-9]+")){
-            return (pin.length() == 4) ? true : (pin.length() == 6) ? true : false;
-        }
-        return false;
+        return pin.matches("[0-9]{4}|[0-9]{6}");
     }
 }
