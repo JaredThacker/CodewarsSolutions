1aadd950d6bcd1b0ca769874e86d373f0a5e6b5e
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-30 11:12:58 -0400

    simple fun 176

---
+++ src/test/java/7Kyu/SimpleFun176Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SimpleFun176Test {
+    @Test
+    public void testSomething() {
+        doTest("krishan", "nahsirk");
+        doTest("ultr53o?n", "nortlu");
+        doTest("ab23c", "cba");
+        doTest("krish21an", "nahsirk");
+    }
+    private void doTest(final String str, final String expected) {
+        assertEquals(expected, SimpleFun176ReverseLetter.reverseLetter(str));
+    }
+}
