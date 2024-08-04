7807d17228e668bed3b17ed21b354ec9bd007cfb
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-04 10:10:24 -0400

    streamify

--- src/main/java/7Kyu/JadenCase.java
+++ src/main/java/7Kyu/JadenCase.java
@@ -1,18 +1,33 @@
+import java.util.Arrays;
+import java.util.stream.*;
+
 public class JadenCase {
 
     public String toJadenCase(String phrase) {
-        if (phrase == null || phrase.isEmpty()) {
+        if (null == phrase || phrase.isEmpty()) {
             return null;
         }
 
-        String[] split = phrase.split(" ");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < split.length; i++){
-            sb.append(Character.toUpperCase(split[i].charAt(0))).append(split[i].substring(1)).append(" ");
-        }
-
-        return sb.toString().trim();
+        return Arrays.stream(phrase.split(" "))
+                .map(e -> e.substring(0, 1).toUpperCase() + e.substring(1))
+                .collect(Collectors.joining(" "));
     }
+
+//    public String toJadenCase(String phrase) {
+//        if (phrase == null || phrase.isEmpty()) {
+//            return null;
+//        }
+//
+//        String[] split = phrase.split(" ");
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < split.length; i++){
+//            sb.append(Character.toUpperCase(split[i].charAt(0)))
+//                    .append(split[i].substring(1))
+//                    .append(" ");
+//        }
+//
+//        return sb.toString().trim();
+//    }
 
 }
